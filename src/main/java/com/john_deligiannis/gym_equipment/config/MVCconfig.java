5686a0c1a8fbf32e5d2ca4179ef2adf5bce46a32
John Delta <john.deligiannis1@gmail.com>
2020-10-28 19:11:05 +0200

    init basic spring webmvc structure. still throws 404

--- src/main/java/com/john_deligiannis/gym_equipment/config/MVCconfig.java
+++ src/main/java/com/john_deligiannis/gym_equipment/config/MVCconfig.java
@@ -5,28 +5,33 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @EnableWebMvc
 @Configuration
-@ComponentScan({"com.john_deligiannis"})
-
+@ComponentScan(basePackages = {"com.john_deligiannis.gym_equipment"})
 public class MVCconfig implements WebMvcConfigurer {
 
     @Bean
     public InternalResourceViewResolver viewResolver(){
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
         viewResolver.setViewClass(JstlView.class);
-        viewResolver.setPrefix("/WEB-INF/views");
+        viewResolver.setPrefix("/WEB-INF/views/");
         viewResolver.setSuffix(".jsp");
         return viewResolver;
     }
     
-    @Override
-    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-        configurer.enable();
-    }
+//    @Override
+//    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+//        configurer.enable();
+//    }
+    
+    @Override 
+    public void addResourceHandlers(ResourceHandlerRegistry registry) { 
+     registry.addResourceHandler("/resources/**").addResourceLocations("/resources/"); 
+    } 
     
 }
--- src/main/java/com/john_deligiannis/gym_equipment/controllers/SampleController.java
+++ src/main/java/com/john_deligiannis/gym_equipment/controllers/SampleController.java
@@ -2,16 +2,17 @@ package com.john_deligiannis.gym_equipment.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SampleController {
 
-	@RequestMapping("/sample")
+	@RequestMapping(value="/sample", method=RequestMethod.GET)
     public ModelAndView showForm() {
 		
 		ModelAndView mv = new ModelAndView(); 
-        mv.setViewName("sample.jsp"); 
+        mv.setViewName("sample"); 
         mv.addObject("result", 
                      "GeeksForGeeks Welcomes "
                          + "you to Spring!"); 
